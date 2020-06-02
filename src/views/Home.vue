e521caa2694d177e62255f7bb9a05719d3c97f39
goberman15 <akbarrmdhn94@gmail.com>
2020-06-03 03:20:54 +0700

    add banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
+  <div class="home d-flex flex-column justify-content-start align-items-center mt-3">
+    <h1 class="balsamiq">Welcome to Gober's Sporting Goods</h1>
+    <Banner/>
     <ProductList/>
   </div>
 </template>
 
 <script>
 import ProductList from '@/components/ProductList.vue'
+import Banner from '@/components/Banner.vue'
 
 export default {
   name: 'Home',
   components: {
-    ProductList
+    ProductList, Banner
   },
   created () {
     this.$store.dispatch('showProducts')
@@ -18,3 +21,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.balsamiq {
+  font-family: 'Balsamiq Sans', cursive;
+}
+</style>
